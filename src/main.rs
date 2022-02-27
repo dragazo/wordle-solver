10cf42eff8a0463382d71c43f66d79cf2986ddbf
Devin Jean <emailcruzjean@yahoo.com>
2022-02-27 03:35:17 -0600

    better cli

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,42 @@
 use wordle_solver::*;
 
+use clap::Parser;
+
 const WORD_LEN: usize = 5;
 
+#[derive(Parser)]
+struct Args {
+    #[clap(short, long, default_value_t = num_cpus::get())]
+    threads: usize,
+
+    inputs: Vec<String>,
+}
+
 fn main() {
-    let args: Vec<_> = std::env::args().collect();
-    let mut guesses = vec![];
+    let args = Args::parse();
+    let mut inputs = vec![];
 
-    for arg in &args[1..] {
-        if ["-h", "--help"].contains(&arg.as_str()) {
-            println!("usage: {} [<guess>:<response>...]\n    <response> is a sequence of 'c' (correct), 'p' (present), and 'a' (absent)", args[0]);
-            std::process::exit(0);
-        }
-
-        let sep = match arg.find(':') {
+    for input in args.inputs.iter() {
+        let sep = match input.find(':') {
             Some(x) => x,
-            None => panic!("unknown input '{}' (expected <guess>:<response>, see -h for info)", arg),
+            None => panic!("unknown input '{}' (expected <guess>:<response>, see -h for info)", input),
         };
-        let guess = &arg[..sep];
-        let response: Vec<_> = arg[sep+1..].chars().map(|ch| match ch {
+        let guess = &input[..sep];
+        let response: Vec<_> = input[sep+1..].chars().map(|ch| match ch {
             'c' => Hint::Correct,
             'p' => Hint::Present,
             'a' => Hint::Absent,
             x => panic!("unknown response '{}' (expected 'c' (correct), 'p' (present), or 'a' (absent))", x),
         }).collect();
-        guesses.push((guess, response));
+        inputs.push((guess, response));
     }
 
     let mut puzzle = Puzzle::new(WORD_LEN);
-    for (guess, response) in guesses.iter() {
+    for (guess, response) in inputs.iter() {
         puzzle.guess(guess, response).unwrap();
     }
 
     println!("input summary:\n{}", puzzle);
-    let (best_guess, worst_rem, avg_rem) = puzzle.best_guess(8).unwrap();
+    let (best_guess, worst_rem, avg_rem) = puzzle.best_guess(args.threads).unwrap();
     println!("best guess: {}\nremaining words: {} worst, {} avg.", best_guess, worst_rem, avg_rem);
 }
